61e246e158e5769b3a0b80fbee8813090f124b79
Navindranath Pohur <pohur95@gmail.com>
2021-04-27 16:41:51 +0400

    horizontal working

--- CustomList/CustomList/App.xaml.cs
+++ CustomList/CustomList/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CustomList.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace CustomList
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new HorizontalCollection();
         }
 
         protected override void OnStart()
--- CustomList/CustomList/Views/HorizontalCollection.xaml.cs
+++ CustomList/CustomList/Views/HorizontalCollection.xaml.cs
@@ -7,17 +7,37 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using static CustomList.Viewmodels.TestListViewModel;
 
 namespace CustomList.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HorizontalCollection : ContentPage
     {
+        bool canSelectItem = false;
         public HorizontalCollection()
         {
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
             BindingContext = new TestListViewModel();
+        }
+
+        private async void ItemSelected(object sender, SelectionChangedEventArgs e)
+        {
+            if (canSelectItem) return;
+            canSelectItem = true;
+            var item = e.CurrentSelection;
+            if (item.Count == 1)
+            {
+                var data = item.FirstOrDefault();
+                if (data != null)
+                {
+                    var dataselected = (Customer)data;
+                    await DisplayAlert("Item Selected", "You have selected item with name: " + dataselected.NAME, "OK");
+                }
+            }
+            canSelectItem = false;
+            ((CollectionView)sender).SelectedItem = null;
         }
     }
 }
